a60435ec10858047c858a46db7ccc33595226427
Vuyo November <vuyonov@gmail.com>
2026-06-24 19:13:37 +0200

    Remove unused Twitter import from Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { motion } from 'framer-motion'
-import { Mail, Instagram, Twitter } from 'lucide-react'
+import { Mail, Instagram } from 'lucide-react'
 
 export default function Footer() {
 
