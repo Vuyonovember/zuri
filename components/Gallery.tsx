e8bbf87e80035ebd26fe6ffdf78e883f3f0bce59
Vuyo November <vuyonov@gmail.com>
2026-06-24 19:42:40 +0200

    Remove unused ShoppingCart import from Gallery

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -4,7 +4,6 @@ import { useState } from 'react'
 import { motion } from 'framer-motion'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
-import { ShoppingCart } from 'lucide-react'
 
 const products = [
   {
